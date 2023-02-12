aa961c1d912fc9ec5acbf20933d2d94b8dea5321
Hirbu Andrei <37737373+andrei-hrb@users.noreply.github.com>
2023-02-12 21:35:58 +0200

    rollback logo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,7 +1,7 @@
 export default function ApplicationLogo({ className }) {
   return (
     <>
-      <h2 className="text-5xl text-center">📸</h2>
+      <img src="/logo.png" alt="Flash's logo" className="w-12" />
       <h1 className="hidden">Flash</h1>
     </>
   )
